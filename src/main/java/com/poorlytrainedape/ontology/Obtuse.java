ce5352738db8dcdc24c76afc6619fbf91ba202c8
Nick Husher <nick.husher@faraday.io>
2015-04-07 12:54:15 -0400

    Add Obtuse interface

---
+++ src/main/java/com/poorlytrainedape/ontology/Obtuse.java
@@ -0,0 +1,9 @@
+package com.poorlytrainedape.ontology;
+
+/**
+ * Attempts to use the Java Reflection API on objects that implement Obtuse
+ * will always receive unhelpful--but technically true--type signatures.
+ */
+public interface Obtuse {
+
+}
